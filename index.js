0b1583becb1a4ad7f195a0dba6e5f63e69371868
asugan <cagatayeren1898@gmail.com>
2022-09-18 22:26:23 +0300

    async

--- index.js
+++ index.js
@@ -13,71 +13,69 @@ puppeteer.use(StealthPlugin());
 const USER_AGENT =
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36";
 
+const items = [];
+
 app.get("/", (req, res) => {
   res.json("Hello Steam API XD");
 });
 
-app.get("/steamdb", (req, res) => {
-  async () => {
-    let browser = await puppeteer.launch({
-      headless: true,
-      executablePath: process.env.CHROME_BIN || null,
-      args: [
-        "--enable-features=NetworkService",
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-      ],
-      ignoreHTTPSErrors: true,
-      dumpio: false,
+app.get("/steamdb", async (req, res) => {
+  let browser = await puppeteer.launch({
+    headless: true,
+    executablePath: process.env.CHROME_BIN || null,
+    args: [
+      "--enable-features=NetworkService",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+    ],
+    ignoreHTTPSErrors: true,
+    dumpio: false,
+  });
+  const [page] = await browser.pages();
+  const userAgent = randomUseragent.getRandom();
+  const UA = userAgent || USER_AGENT;
+
+  //Randomize viewport size
+  await page.setViewport({
+    width: 1920 + Math.floor(Math.random() * 100),
+    height: 3000 + Math.floor(Math.random() * 100),
+    deviceScaleFactor: 1,
+    hasTouch: false,
+    isLandscape: false,
+    isMobile: false,
+  });
+
+  await page.setUserAgent(UA);
+  await page.setJavaScriptEnabled(true);
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto("https://steamdb.info/patchnotes/", {
+    waitUntil: "networkidle0",
+  });
+  const data = await page.evaluate(() => document.querySelector("*").outerHTML);
+
+  const mydata = cheerio.load(data);
+  const items = [];
+  mydata("table > tbody > tr").map((i, el) => {
+    const tds = mydata(el).find("td");
+    const date = mydata(el).find(tds[0]).text().replace(/\n/g, "");
+    const image = mydata(el).find(tds[1]).find("img").attr("src");
+    const game_name = mydata(el).find(tds[2]).text().replace(/\n/g, "");
+    const note = mydata(el).find(tds[3]).text().replace(/\n/g, "");
+    const parts = image.split("/");
+    const appid = parts[parts.length - 2];
+
+    items.push({
+      date,
+      game_name,
+      note,
+      image,
+      appid,
     });
-    const [page] = await browser.pages();
-    const userAgent = randomUseragent.getRandom();
-    const UA = userAgent || USER_AGENT;
+  });
 
-    //Randomize viewport size
-    await page.setViewport({
-      width: 1920 + Math.floor(Math.random() * 100),
-      height: 3000 + Math.floor(Math.random() * 100),
-      deviceScaleFactor: 1,
-      hasTouch: false,
-      isLandscape: false,
-      isMobile: false,
-    });
+  const atmis = items.slice(0, 40);
 
-    await page.setUserAgent(UA);
-    await page.setJavaScriptEnabled(true);
-    await page.setDefaultNavigationTimeout(0);
-    await page.goto("https://steamdb.info/patchnotes/", {
-      waitUntil: "networkidle0",
-    });
-    const data = await page.evaluate(
-      () => document.querySelector("*").outerHTML
-    );
-
-    const mydata = cheerio.load(data);
-    const items = [];
-    mydata("table > tbody > tr").map((i, el) => {
-      const tds = mydata(el).find("td");
-      const date = mydata(el).find(tds[0]).text().replace(/\n/g, "");
-      const image = mydata(el).find(tds[1]).find("img").attr("src");
-      const game_name = mydata(el).find(tds[2]).text().replace(/\n/g, "");
-      const note = mydata(el).find(tds[3]).text().replace(/\n/g, "");
-      const parts = image.split("/");
-      const appid = parts[parts.length - 2];
-
-      items.push({
-        date,
-        game_name,
-        note,
-        image,
-        appid,
-      });
-    });
-
-    const atmis = items.slice(0, 50);
-
-    res.json(atmis);
-    await browser.close();
-  };
+  res.json(atmis);
+  await browser.close();
 });
